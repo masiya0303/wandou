acb35238801896889a5140923dff7a3e794333d0
root <root@localhost.localdomain>
2026-06-12 01:59:02 +0800

    fix: WorldListScreen back button now returns to main menu

--- src/App.vue
+++ src/App.vue
@@ -19,7 +19,7 @@ const showSettings = ref(false)
 <template>
   <div class="app-root">
     <StartScreen v-if="store.phase === 'start'" @open-settings="showSettings = true" />
-    <WorldListScreen v-else-if="store.phase === 'worldList'" />
+    <WorldListScreen v-else-if="store.phase === 'worldList'" @back="store.phase = 'start'" />
     <WorldDetailScreen v-else-if="store.phase === 'worldDetail'" />
     <SetupScreen v-else-if="store.phase === 'setup'" />
     <GameMain v-else-if="store.phase === 'playing'" @open-settings="showSettings = true" />
